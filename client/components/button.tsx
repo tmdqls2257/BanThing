5fdcbb163ab7b5f3278a0bc9c2261407b5f65767
tmdqls2257 <54207332+tmdqls2257@users.noreply.github.com>
2022-02-17 14:21:19 +0900

    [ADD]: 메인페이지의 지도와 사이드바 구현

---
+++ client/components/button.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+interface buttonType {
+  disabled?: boolean
+  children: string
+}
+
+function Button({ disabled, children }: buttonType) {
+  return <StyledButton disabled={disabled}>{children}</StyledButton>
+}
+
+const StyledButton = styled.button`
+  margin: 0;
+  border: none;
+  cursor: pointer;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 24px;
+  padding: 12px 16px;
+  border-radius: 50px;
+  color: #ffffff;
+  background-color: #ff8a3d;
+`
+
+export default Button
