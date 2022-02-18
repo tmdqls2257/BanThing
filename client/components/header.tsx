85e5b198db275377131381b593cba0c95a498bfb
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2022-02-18 11:04:23 +0900

    [Client] FIX: 반응형 CSS 미적용 현상 해결

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -11,31 +11,15 @@ export default function NavBar() {
           </a>
         </Link>
 
-        <ul className={styles.nav}>
-          <li>
-            <Link href="/">
-              <a className={styles.nav_menu}>HOME</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/main">
-              <a className={styles.nav_menu}>MAIN</a>
-            </Link>
-          </li>
-          <li>
-            <a className={styles.nav_menu}>LOGIN</a>
-          </li>
-        </ul>
-
-        {/* <nav className={styles.nav}>
+        <nav className={styles.nav}>
           <Link href="/">
             <a className={styles.nav_menu}>HOME</a>
           </Link>
-          <a className={styles.nav_menu}>|</a>
-          <Link href="main">
+          <Link href="/main">
             <a className={styles.nav_menu}>MAIN</a>
           </Link>
-        </nav> */}
+          <a className={styles.nav_menu}>LOGIN</a>
+        </nav>
       </div>
     </>
   );
--- client/components/home/title.tsx
+++ client/components/home/title.tsx
@@ -1,35 +1,53 @@
 import Link from "next/link";
+import styled from "styled-components";
 import styles from "../../styles/Home.module.css";
-import { Fade, AttentionSeeker } from "react-awesome-reveal";
+import { Zoom, Fade } from "react-awesome-reveal";
+
+export const Container = styled.div`
+  width: 89vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+`;
 
 export default function Title() {
   return (
     <>
-      <div className={styles.title}>
+      <Container>
         <main className={styles.main}>
-          <h1 className={styles.title_title}>
-            <span className={styles.orange}>Ban</span>
-            <span className={styles.black}>Thing</span>
-          </h1>
-          <div className={styles.title_description}>
-            <span>부담을 </span>
-            <span className={styles.orange}>나눠 </span>
-            <span>드세요.</span>
-          </div>
-          <Link href="/main">
-            <button className={styles.title_button}>
-              <span>시작하기 </span>
-            </button>
-          </Link>
+          <Zoom triggerOnce={true}>
+            <div className={styles.title_title}>
+              <span className={styles.orange}>Ban</span>
+              <span className={styles.black}>Thing</span>
+            </div>
+          </Zoom>
+          <Fade triggerOnce={true} delay={1000}>
+            <div className={styles.title_description}>
+              <span>부담을 </span>
+              <span className={styles.orange}>나눠 </span>
+              <span>드세요.</span>
+            </div>
+          </Fade>
+          <Fade triggerOnce={true} delay={2000}>
+            <Link href="/main">
+              <button className={styles.title_button}>
+                <span>시작하기 </span>
+              </button>
+            </Link>
+          </Fade>
         </main>
-        <div>
-          <img
-            src="/title.png"
-            alt="title-image"
-            className={styles.title_image}
-          />
-        </div>
-      </div>
+        <Fade triggerOnce={true} duration={1200}>
+          <div className={styles.title_image_container}>
+            <img
+              src="/title.png"
+              alt="title-image"
+              className={styles.title_image}
+            />
+          </div>
+        </Fade>
+      </Container>
     </>
   );
 }
