c2fef8dd411a09f7b74f43906b778da3520e1435
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2022-02-18 13:34:51 +0900

    [Client] UPDATE: logo 이미지 png 파일에서 svg 파일로 변경

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
       <div className={styles.header}>
         <Link href="/main">
           <a className={styles.logo}>
-            <img src="Logo.png" alt="Vercel Logo" />
+            <img src="banthing.svg" alt="BanThing Logo" />
           </a>
         </Link>
 
--- client/components/home/title.tsx
+++ client/components/home/title.tsx
@@ -1,36 +1,26 @@
 import Link from "next/link";
-import styled from "styled-components";
 import styles from "../../styles/Home.module.css";
 import { Zoom, Fade } from "react-awesome-reveal";
-
-export const Container = styled.div`
-  width: 89vw;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  margin: auto;
-`;
 
 export default function Title() {
   return (
     <>
-      <Container>
+      <div className={styles.home_component_container}>
         <main className={styles.main}>
-          <Zoom triggerOnce={true}>
+          <Zoom triggerOnce={true} delay={150}>
             <div className={styles.title_title}>
               <span className={styles.orange}>Ban</span>
               <span className={styles.black}>Thing</span>
             </div>
           </Zoom>
-          <Fade triggerOnce={true} delay={800}>
+          <Fade triggerOnce={true} delay={1100}>
             <div className={styles.title_description}>
               <span>부담을 </span>
-              <span className={styles.orange}>나눠 </span>
-              <span>드세요.</span>
+              <span className={styles.orange}>반띵</span>
+              <span>하세요.</span>
             </div>
           </Fade>
-          <Fade triggerOnce={true} delay={1500}>
+          <Fade triggerOnce={true} delay={1800}>
             <Link href="/main">
               <button className={styles.title_button}>
                 <span>시작하기 </span>
@@ -47,7 +37,7 @@ export default function Title() {
             />
           </div>
         </Fade>
-      </Container>
+      </div>
     </>
   );
 }
