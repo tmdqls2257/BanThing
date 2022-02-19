97aa05ef7bb44262a114519452a09650a69db61a
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2022-02-19 22:15:42 +0900

    [Client] FEAT: Login, SignUp, My Page 구현 완료했습니다.

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,7 +1,15 @@
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import Login from './login';
+import React, { useState } from 'react';
 
 export default function NavBar() {
+  const [loginModal, setLoginModal] = useState(false);
+
+  const openLoginModal = () => {
+    setLoginModal(true);
+  };
+
   return (
     <>
       <div className={styles.header}>
@@ -18,9 +26,17 @@ export default function NavBar() {
           <Link href="/main">
             <a className={styles.nav_menu}>MAIN</a>
           </Link>
-          <a className={styles.nav_menu}>LOGIN</a>
+          <a className={styles.nav_menu} onClick={() => openLoginModal()}>
+            LOGIN
+          </a>
         </nav>
       </div>
+
+      {loginModal ? (
+        <Login loginModal={loginModal} setLoginModal={setLoginModal} />
+      ) : (
+        <></>
+      )}
     </>
   );
 }
--- client/components/login.tsx
+++ client/components/login.tsx
@@ -1,9 +1,61 @@
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Login.module.css';
+import SignUp from './signup';
+import React, { useState } from 'react';
+interface propsType {
+  loginModal: boolean;
+  setLoginModal: Function;
+}
 
-export default function Login() {
+export default function Login(prop: propsType) {
+  const [signUpModal, setSignUpModal] = useState(false);
+
+  const openSignUpModal = () => {
+    setSignUpModal(true);
+  };
+
   return (
     <>
-      <div>YEAH</div>
+      <div className={styles.login_container}>
+        <div className={styles.login_modal}>
+          <span
+            className={styles.login_times}
+            onClick={() => prop.setLoginModal(false)}
+          >
+            &times;
+          </span>
+          <div className={styles.login_title}>
+            Ban<span className={styles.black}>Thing</span>
+          </div>
+          <input
+            className={styles.login_input_box}
+            placeholder="아이디"
+          ></input>
+          <input
+            className={styles.login_input_box}
+            placeholder="비밀번호"
+          ></input>
+          <span className={styles.login_error}>
+            아이디 또는 비밀번호가 일치하지 않습니다.
+          </span>
+          <button className={styles.login_button}>로그인</button>
+          <div className={styles.login_description}>
+            계정이 없으신가요?{' '}
+            <span
+              className={styles.login_signup}
+              onClick={() => openSignUpModal()}
+            >
+              회원가입
+            </span>
+          </div>
+        </div>
+        {signUpModal ? (
+          <>
+            <SignUp signUpModal={signUpModal} setSignUpModal={setSignUpModal} />
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </>
   );
 }
