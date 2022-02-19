2cdea60d0c3d022a667f1ca693fe4b00e07d0e46
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2022-02-19 11:32:24 +0900

    [Client] UPDATE: Home Page 애니메이션 수정

--- client/components/home/text.tsx
+++ client/components/home/text.tsx
@@ -4,7 +4,7 @@ import { Bounce } from 'react-awesome-reveal';
 export default function Text() {
   return (
     <>
-      <Bounce triggerOnce={true} delay={300} duration={800}>
+      <Bounce triggerOnce={true} delay={900} duration={1000}>
         <div className={styles.text_container}>
           <div>배달메이트와 함께 주문하여</div>
           <div>배달음식에 대한 부담을 줄여보세요!</div>
