40ae25f62c4d6e200b63143c254cb5753f75ecc8
minho0513 <minho6474@gmail.com>
2022-02-20 15:29:12 +0900

    [Server] UPDATE: 비밀번호 변경 기능 구현

--- server/src/mypage/mypage.controller.ts
+++ server/src/mypage/mypage.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req, Post, Body } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthGuard } from 'src/token/auth.guard';
 import { MypageService } from './mypage.service';
+import { TransPasswordDTO } from 'src/dto/transpassword.dto';
 
 @Controller('mypage')
 export class MypageController {
@@ -11,5 +12,14 @@ export class MypageController {
   @UseGuards(AuthGuard)
   async userInfo(@Req() req: Request): Promise<object> {
     return await this.myPageService.userInfo(req.user);
+  }
+
+  @Post() //비밀번호 변경
+  @UseGuards(AuthGuard)
+  async editPassword(
+    @Req() req: Request,
+    @Body() password: TransPasswordDTO,
+  ): Promise<any> {
+    return await this.myPageService.editPassword(req.user, password);
   }
 }
