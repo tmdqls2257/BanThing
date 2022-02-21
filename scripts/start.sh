4641c42897ea4765727fdfcf1a436a9e44b4a815
shren207 <shren0812@gmail.com>
2022-02-21 15:24:56 +0900

    [Server] ADD: start.sh 에 환경변수 JWT_SECRETKEY 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,7 @@ export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USERNAME --query Parameters[0].Value | sed 's/"//g')
+export JWT_SECRETKEY=$(aws ssm get-parameters --region ap-northeast-2 --names JWT_SECRETKEY --query Parameters[0].Value | sed 's/"//g')
 
 npm run build
 authbind --deep pm2 start dist/main.js
