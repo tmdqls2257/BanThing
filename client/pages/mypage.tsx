7f79d579048afa0fcc3c96dd53f587d1adfa18ce
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2022-02-21 21:41:11 +0900

    [Client] FEAT: My Page 유효성 검사 구현 완료

--- client/pages/mypage.tsx
+++ client/pages/mypage.tsx
@@ -1,8 +1,50 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/MyPage.module.css';
+import { useState } from 'react';
 
 const MyPage: NextPage = () => {
+  const isSmallLetterAndNumber4to10 = /^[a-zA-z0-9]{4,10}$/;
+
+  const [changePassword, setChangePassword] = useState('');
+  const [checkPassword, setCheckPassword] = useState('');
+
+  const [correctChangePassword, setCorrectChangePassowrd] = useState(true);
+  const [changeEqualCheck, setChangeEqualCheck] = useState(true);
+
+  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChangePassword(event.target.value);
+  };
+
+  const handleCheckPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckPassword(event.target.value);
+  };
+
+  const handleBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const type = event.target.id;
+
+    if (type === 'change_password') {
+      if (!value) {
+        setCorrectChangePassowrd(true);
+      } else if (isSmallLetterAndNumber4to10.test(value)) {
+        setCorrectChangePassowrd(true);
+      } else {
+        setCorrectChangePassowrd(false);
+      }
+    }
+
+    if (type === 'check_password') {
+      if (!value) {
+        setChangeEqualCheck(true);
+      } else if (changePassword === checkPassword) {
+        setChangeEqualCheck(true);
+      } else {
+        setChangeEqualCheck(false);
+      }
+    }
+  };
+
   return (
     <>
       <Head>
@@ -38,23 +80,39 @@ const MyPage: NextPage = () => {
               />
             </div>
             <input
+              id="change_password"
               className={styles.mypage_password_change_check}
               placeholder="변경할 비밀번호 입력"
               type="password"
+              onChange={handleChangePassword}
+              onBlur={handleBlur}
             />
-            <span className={styles.mypage_error}>
-              4~10자 영문, 숫자를 사용하세요.
-            </span>
-            {/* <span className={styles.mypage_space}>_</span> */}
+            {correctChangePassword ? (
+              <span className={styles.mypage_space}>
+                4~10자 영문, 숫자를 사용하였습니다.
+              </span>
+            ) : (
+              <span className={styles.mypage_error}>
+                4~10자 영문, 숫자를 사용하세요.
+              </span>
+            )}
             <input
+              id="check_password"
               className={styles.mypage_password_change_check}
               placeholder="변경할 비밀번호 확인"
               type="password"
+              onChange={handleCheckPassword}
+              onBlur={handleBlur}
             />
-            <span className={styles.mypage_error}>
-              비밀번호가 일치하지 않습니다.
-            </span>
-            {/* <span className={styles.mypage_space}>_</span> */}
+            {changeEqualCheck ? (
+              <span className={styles.mypage_space}>
+                비밀번호가 일치합니다.
+              </span>
+            ) : (
+              <span className={styles.mypage_error}>
+                비밀번호가 일치하지 않습니다.
+              </span>
+            )}
           </div>
           <div className={styles.mypage_button_container}>
             <button className={styles.mypage_modify_button}>수정하기</button>
