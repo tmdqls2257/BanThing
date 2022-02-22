3a8aa16970fed0bba35db40d06092bb41525c15a
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2022-02-22 22:56:18 +0900

    [Client] UPDATE: Login 기능 구현중

--- client/components/login.tsx
+++ client/components/login.tsx
@@ -15,6 +15,8 @@ export default function Login(prop: propsType) {
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
 
+  const [loginMessage, setLoginMessage] = useState('');
+
   const openSignUpModal = () => {
     setSignUpModal(true);
   };
@@ -28,20 +30,26 @@ export default function Login(prop: propsType) {
   };
 
   const handleLogin = () => {
-    axios
-      .post(`http://${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/users/login`, {
-        user_id: userId,
-        password: password,
-      })
-      .then((response) => {
-        const { accessToken } = response.data.data;
-        localStorage.setItem('accessToken', accessToken);
-        console.log(accessToken);
-        prop.setAccessToken(accessToken);
-        prop.setIsLogin(true);
-        prop.setLoginModal(false);
-      })
-      .catch((error) => console.log(error));
+    if (userId === '' || password === '') {
+      setLoginMessage('아이디와 비밀번호를 모두 입력해주세요.');
+    } else {
+      axios
+        .post(`http://${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/users/login`, {
+          user_id: userId,
+          password: password,
+        })
+        .then((response) => {
+          const { accessToken } = response.data.data;
+          localStorage.setItem('accessToken', accessToken);
+          prop.setAccessToken(accessToken);
+          prop.setIsLogin(true);
+          prop.setLoginModal(false);
+        })
+        .catch((error) => {
+          if (error)
+            setLoginMessage('아이디 또는 비밀번호가 일치하지 않습니다.');
+        });
+    }
   };
 
   return (
@@ -68,9 +76,7 @@ export default function Login(prop: propsType) {
             placeholder="비밀번호"
             onChange={handlePassword}
           ></input>
-          <span className={styles.login_error}>
-            아이디 또는 비밀번호가 일치하지 않습니다.
-          </span>
+          <span className={styles.login_error}>{loginMessage}</span>
           <button className={styles.login_button} onClick={handleLogin}>
             <img
               src="/login.png"
