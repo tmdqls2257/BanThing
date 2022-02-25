fb8223e3aa2be8020eca4a17d38f145172c58293
shren207 <shren0812@gmail.com>
2022-02-25 21:35:20 +0900

    [Server] FIX: 서버 포트번호 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,4 +9,4 @@ export DATABASE_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --name
 export JWT_SECRETKEY=$(aws ssm get-parameters --region ap-northeast-2 --names JWT_SECRETKEY --query Parameters[0].Value | sed 's/"//g')
 
 npm run build
-authbind --deep pm2 start dist/main.js --watch
+authbind --deep pm2 start dist/main.js
