bf2e0e537558315dd081048942dd031663aba5bb
Seung Bin Hong <tmdqls2257@gmail.com>
2022-02-25 14:54:37 +0900

    [Client] FIX: room룸 에러 수정

--- client/components/main/joinRoom.tsx
+++ client/components/main/joinRoom.tsx
@@ -1,7 +1,7 @@
 import Button from './button';
 import styled from 'styled-components';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 
 const Container = styled.div`
   /* 컴포넌트를 보고 싶다면 display: flex; 바꿔주세요 */
