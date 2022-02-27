35fb83af62ed636f3e6331d132310c664b6d6649
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2022-02-27 17:22:17 +0900

    [Client] UPDATE: Landing Page 디자인 수정

--- client/pages/testForLogin.tsx
+++ client/pages/testForLogin.tsx
@@ -10,44 +10,60 @@ import {
   Roll,
   Rotate,
 } from 'react-awesome-reveal';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  min-height: 50vw;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function test() {
   return (
     <>
-      <Fade>
-        <h1>FADE</h1>
-      </Fade>
+      <Container>
+        <Fade>
+          <h1>FADE</h1>
+        </Fade>
 
-      <Bounce>
-        <h1>BOUNCE</h1>
-      </Bounce>
+        <Bounce>
+          <h1>BOUNCE</h1>
+        </Bounce>
 
-      <Zoom>
-        <h1>ZOOM</h1>
-      </Zoom>
+        <Zoom>
+          <h1>ZOOM</h1>
+        </Zoom>
 
-      <Slide>
-        <h1>SLIDE</h1>
-      </Slide>
+        <Slide>
+          <h1>SLIDE</h1>
+        </Slide>
 
-      <Flip>
-        <h1>FLIP</h1>
-      </Flip>
-      <AttentionSeeker>
-        <h1>AttentionSeeker</h1>
-      </AttentionSeeker>
-      <Hinge>
-        <h1>Hinge</h1>
-      </Hinge>
-      <JackInTheBox>
-        <h1>JackInTheBox</h1>
-      </JackInTheBox>
-      <Roll>
-        <h1>Roll</h1>
-      </Roll>
-      <Rotate>
-        <h1>Rotate</h1>
-      </Rotate>
+        <Flip>
+          <h1>FLIP</h1>
+        </Flip>
+
+        <AttentionSeeker>
+          <h1>AttentionSeeker</h1>
+        </AttentionSeeker>
+
+        <Hinge>
+          <h1>Hinge</h1>
+        </Hinge>
+
+        <JackInTheBox>
+          <h1>JackInTheBox</h1>
+        </JackInTheBox>
+
+        <Roll>
+          <h1>Roll</h1>
+        </Roll>
+
+        <Rotate>
+          <h1>Rotate</h1>
+        </Rotate>
+      </Container>
     </>
   );
 }
