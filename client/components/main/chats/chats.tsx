7061adcb1e660ddf981a9d1bb0ef5d604f68546e
shren207 <shren0812@gmail.com>
2022-03-01 12:59:30 +0900

    [Common] RESTORE: merge 오류 복귀

---
+++ client/components/main/chats/chats.tsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+import styles from './chats.module.css';
+import Chat from '../chat/chat';
+import styled from 'styled-components';
+import NewChat from '../newChat/newChat';
+
+const Container = styled.div`
+  overflow-y: scroll;
+  min-height: 50px;
+  max-height: 70vh;
+  ::-webkit-scrollbar {
+    width: 0;
+  }
+  @media screen and (max-height: 480px) {
+    max-height: 120vh;
+  }
+  @media screen and (max-height: 580px) {
+    max-height: 100vh;
+  }
+  @media screen and (max-height: 680px) {
+    max-height: 80vh;
+  }
+`;
+
+interface usersChats {
+  data: {
+    replyLog: [
+      {
+        id: number;
+        nickname: string;
+        post_id: number;
+        reply: string;
+        time: string;
+      },
+    ];
+  };
+}
+interface ChatsType {
+  addable: boolean;
+  roomsId: number;
+  usersChats: usersChats | undefined;
+  usernickname: string;
+}
+
+const Chats = ({ usersChats, roomsId, addable, usernickname }: ChatsType) => {
+  const [userchat, setChat] = useState<string[]>([]);
+
+  const onCreated = (chat: string) => {
+    usersChats?.data.replyLog.push({
+      id: usersChats?.data.replyLog.length + 1,
+      nickname: usernickname,
+      post_id: 1,
+      reply: chat,
+      time: String(new Date()),
+    });
+    setChat((chats) => [...chats, chat]);
+  };
+
+  useEffect(() => {
+    const chatContainer = document.querySelector(
+      '#chat-Container',
+    )! as HTMLElement;
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+  }, [userchat.length]);
+
+  return (
+    <>
+      <section className={styles.chat_container} id={'chat-Container'}>
+        {usersChats?.data.replyLog ? (
+          usersChats?.data.replyLog.map((chat) => (
+            <Chat
+              key={chat.id}
+              owner={usernickname === chat.nickname}
+              chats={chat.reply}
+              nickname={chat.nickname}
+              time={chat.time}
+            />
+          ))
+        ) : (
+          <></>
+        )}
+      </section>
+      {addable && <NewChat onCreated={onCreated} roomsId={roomsId} />}
+    </>
+  );
+};
+
+export default Chats;
---
+++ server/src/app.module.ts
@@ -0,0 +1,42 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './entity/users.entity';
+import { PostModule } from './post/post.module';
+import { Post } from './entity/post.entity';
+import { Reply } from './entity/reply.entity';
+import { MypageModule } from './mypage/mypage.module';
+import { MainPageModule } from './main-page/main-page.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Users, Post, Reply],
+      synchronize: true, //테이블을 자동으로 생성 개발모드일때만 사용 운영모드일때는 삭제
+    }),
+    AuthModule,
+    PostModule,
+    MypageModule,
+    MainPageModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*'); //middleware들은 consumer에다가 연결한다!
+  }
+}
