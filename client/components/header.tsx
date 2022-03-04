d243923c678fd334255e106fc4efece7397f6d87
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2022-03-04 21:36:22 +0900

    [Client] UPDATE: 로고 클릭시 렌더링 최종 변경

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -180,7 +180,7 @@ export default function Header(prop: propsType) {
         // --------- ▼ 로그인이 되어있지 않은 경우 ---------
         <>
           <div className={styles.header}>
-            <Link href="/main">
+            <Link href="/">
               <a className={styles.logo}>
                 <img src="/banthing.png" alt="BanThing Logo" />
               </a>
