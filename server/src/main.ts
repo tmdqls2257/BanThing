695e606dd7eb0ab17c3872a0456fdfa256257285
minho0513 <minho6474@gmail.com>
2022-03-15 14:47:22 +0900

    [Server] UPDATE: 실시간통신

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SocketIoAdapter } from './adapters/socket-io.adapters';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.enableCors({
     origin: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
-  await app.listen(process.env.SERVER_PORT);
+  app.useWebSocketAdapter(new SocketIoAdapter(app));
+  await app.listen(3000);
 }
 bootstrap();
